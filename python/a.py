aba7a7171d8512db5603cccec642fb210b768864
YuJack <jk82421@gmail.com>
2023-08-17 19:44:21 +0800

    remove

--- python/a.py
+++
@@ -1,4 +0,0 @@
-from cool import cool_func
-
-print('Call it remotely')
-cool_func()
--- python/cool.py
+++
@@ -1,6 +0,0 @@
-def cool_func():
-    print('cool_func(): Super Cool!')
-
-if __name__ == '__main__':
-    print('Call it locally')
-    cool_func()
